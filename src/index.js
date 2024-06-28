797b694ce64fabe4f46ef20d627ee18e161f9b4c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-28 21:11:29 +0900

    Conditional Rendering With Ternaries

--- src/index.js
+++ src/index.js
@@ -73,18 +73,23 @@ function Header() {
 
 function Menu() {
 	const pizzas = pizzaData;
+	// const pizzas = [];
 	const numPizzas = pizzas.length;
 
 	return (
 		<main className="menu">
 			<h2>Our Menu</h2>
 
-			{numPizzas > 0 && (
+			{numPizzas > 0 ? (
 				<ul className="pizzas">
 					{pizzas.map((pizza) => (
 						<Pizza pizzaObj={pizza} key={pizza.name} />
 					))}
 				</ul>
+			) : (
+				<p>
+					We're still working on our menu. Please come back later :)
+				</p>
 			)}
 
 			{/* <Pizza
@@ -113,12 +118,12 @@ function Pizza(props) {
 function Footer() {
 	const hour = new Date().getHours();
 	const openHour = 12;
-	const closeHour = 20;
+	const closeHour = 21;
 	const isOpen = hour >= openHour && hour <= closeHour;
 
 	return (
 		<footer className="footer">
-			{isOpen && (
+			{isOpen ? (
 				<div className="order">
 					<p>
 						We're open until {closeHour}:00. Come visit us or order
@@ -126,6 +131,11 @@ function Footer() {
 					</p>
 					<button className="btn">Order</button>
 				</div>
+			) : (
+				<p>
+					We're happy to welcome you between {openHour}:00 and
+					{closeHour}:00.
+				</p>
 			)}
 		</footer>
 	);
