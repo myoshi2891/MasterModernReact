da06ff02dab388aee45fcf83ebf679965b7c84c5
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-28 21:42:50 +0900

    React Fragments

--- src/index.js
+++ src/index.js
@@ -81,11 +81,19 @@ function Menu() {
 			<h2>Our Menu</h2>
 
 			{numPizzas > 0 ? (
-				<ul className="pizzas">
-					{pizzas.map((pizza) => (
-						<Pizza pizzaObj={pizza} key={pizza.name} />
-					))}
-				</ul>
+				<>
+					<p>
+						Authetic Italian cuisine. 6 creative dishes to choose
+						from. All from our stone oven, all organic, all
+						delicious
+					</p>
+
+					<ul className="pizzas">
+						{pizzas.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</>
 			) : (
 				<p>
 					We're still working on our menu. Please come back later :)
