d090b61210be960a3eb32706d487be2c1fd6ad00
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-29 16:17:39 +0900

    Updating State Based on Current State

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ export default function App() {
 	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
-		if (step > 1) setStep(step - 1);
+		if (step > 1) setStep((step) => step - 1);
 	}
 
 	function handleNext() {
-		if (step < 3) setStep(step + 1);
+		if (step < 3) setStep((step) => step + 1);
 	}
 
 	return (
 		<>
-			<button className="close" onClick={() => setIsOpen(!isOpen)}>
+			<button
+				className="close"
+				onClick={() => setIsOpen((isOpen) => !isOpen)}
+			>
 				&times;
 			</button>
+
 			{isOpen && (
 				<div className="steps">
 					<div className="numbers">
