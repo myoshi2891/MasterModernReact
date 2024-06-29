44326c86726887640c86c938abb28d1254c3c770
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-29 16:59:34 +0900

    Building the Layout

--- src/App.js
+++ src/App.js
@@ -1,66 +1,36 @@
-import { useState } from "react";
-
-const messages = [
-	"Learn React ⚛️",
-	"Apply for jobs 💼",
-	"Invest your new income 🤑",
-];
-
 export default function App() {
-	const [step, setStep] = useState(1);
-	const [isOpen, setIsOpen] = useState(true);
-
-	function handlePrevious() {
-		if (step > 1) setStep((step) => step - 1);
-	}
-
-	function handleNext() {
-		if (step < 3) setStep((step) => step + 1);
-	}
-
 	return (
-		<>
-			<button
-				className="close"
-				onClick={() => setIsOpen((isOpen) => !isOpen)}
-			>
-				&times;
-			</button>
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
 
-			{isOpen && (
-				<div className="steps">
-					<div className="numbers">
-						<div className={step >= 1 ? "active" : ""}>1</div>
-						<div className={step >= 2 ? "active" : ""}>2</div>
-						<div className={step >= 3 ? "active" : ""}>3</div>
-					</div>
+function Logo() {
+	return <h1>🏝️ Far Away 🌴</h1>;
+}
 
-					<p className="message">
-						Step {step}: {messages[step - 1]}
-					</p>
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your 😍 trip?</h3>
+		</div>
+	);
+}
 
-					<div className="buttons">
-						<button
-							style={{
-								backgroundColor: "#7950f2",
-								color: "#fff",
-							}}
-							onClick={handlePrevious}
-						>
-							Previous
-						</button>
-						<button
-							style={{
-								backgroundColor: "#7950f2",
-								color: "#fff",
-							}}
-							onClick={handleNext}
-						>
-							Next
-						</button>
-					</div>
-				</div>
-			)}
-		</>
+function PackingList() {
+	return <div className="list">List</div>;
+}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>
+				🤟You have X items on your list, and you already packed X (X%)
+			</em>
+		</footer>
 	);
 }
