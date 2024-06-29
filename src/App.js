fc363759e7e5d04997653c4573ae734c78632d9a
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-29 15:25:32 +0900

    Handling Events the React Way

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
 	const step = 1;
 
+	function handlePrevious() {
+		alert("testing");
+	}
+
+	function handleNext() {
+		alert("testing");
+	}
+
 	return (
 		<div className="steps">
 			<div className="numbers">
@@ -14,14 +22,21 @@ export default function App() {
 				<div className={`${step >= 2 ? "active" : ""} `}>2</div>
 				<div className={`${step >= 3 ? "active" : ""} `}>3</div>
 			</div>
+
 			<p className="message">
 				Step {step}: {messages[step - 1]}
 			</p>
 			<div className="buttons">
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+				<button
+					style={{ backgroundColor: "#7950f2", color: "#fff" }}
+					onClick={handlePrevious}
+				>
 					Previous
 				</button>
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+				<button
+					style={{ backgroundColor: "#7950f2", color: "#fff" }}
+					onClick={handleNext}
+				>
 					Next
 				</button>
 			</div>
