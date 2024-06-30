2a2484afc1fddaa54c309f1252deb74c17790148
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-06-30 13:44:24 +0900

    Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
 			/>
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 }
@@ -117,11 +117,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 	);
 }
 
-function Stats() {
+function Stats({ items }) {
+	if (!items.length)
+		return (
+			<p className="stats">
+				<em>Start adding some items to your packing list 🤬</em>
+			</p>
+		);
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const percentage = Math.round((numPacked / numItems) * 100);
+
 	return (
 		<footer className="stats">
 			<em>
-				🤟You have X items on your list, and you already packed X (X%)
+				{percentage === 100
+					? "You got everything! Ready to go 🛩️"
+					: `🤟You have ${numItems} items on your list, and you already packed
+				${numPacked} (${percentage}%)`}
 			</em>
 		</footer>
 	);
